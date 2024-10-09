404302d530c891ce4ae8e2005d418f899ca5fcd9
GoldSenseiBoi <98558347+GoldSenseiBoi@users.noreply.github.com>
2024-10-10 00:35:01 +0200

    add :swimmer:

--- js/anime_Gsap.js
+++ js/anime_Gsap.js
@@ -1,11 +1,47 @@
 gsap.to(".circle", {
      y : 300,
     duration : 5,
-delay: 2,
-ease : "elastic",
-repeat :  2,
+delay: 1,
+repeat :  -1,
 yoyo : true,
-onUpdate : () => {
+/*onUpdate : () => {
     document.querySelector(".circle img").setAttribute("src", "images/eggs.png")
-}
+    },*/
+    stagger : 0.1,
+})
+
+gsap.to(".circle1", {
+    y : 300,
+   duration : 5,
+delay: 3,
+repeat :  -1,
+yoyo : true,
+/*onUpdate : () => {
+   document.querySelector(".circle img").setAttribute("src", "images/eggs.png")
+   },*/
+   stagger : 0.1,
+})
+
+gsap.to(".circle2", {
+    y : 300,
+   duration : 5,
+delay: 5,
+repeat :  -1,
+yoyo : true,
+/*onUpdate : () => {
+   document.querySelector(".circle img").setAttribute("src", "images/eggs.png")
+   },*/
+   stagger : 0.1,
+})
+
+gsap.to(".circle3", {
+    y : 300,
+   duration : 5,
+delay: 7,
+repeat :  -1,
+yoyo : true,
+/*onUpdate : () => {
+   document.querySelector(".circle img").setAttribute("src", "images/eggs.png")
+   },*/
+   stagger : 0.1,
 })
